b9881a3389b5c3113723e987e40d835963d3ad31
kostyklv <kostyklv@inbox.lv>
2021-12-09 20:59:42 +0200

    Homework2

--- main.js
+++ main.js
@@ -1,4 +1,35 @@
-console.log('Fight!');
+const player1 = {
+    name:'Scorpion',
+    hp: 100,
+    image:'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+    weapon:['sable','axe','sword'],
+    attack: function (name) {
+        console.log(name + 'Fight...');
+    }
+};
 
-const firstName ='Sonya';
-console.log(typeof firstName);
+const player2 = {
+    name:'Sonya',
+    hp: 100,
+    image:'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
+    weapon:['sable','axe','sword'],
+    attack: function () {
+        console.log(name + 'Fight...');
+    }
+};
+
+const $box = document.getElementById('box');
+console.log($box.innerText);
+function createPlayer() {
+
+
+};
+
+player1.attack(player1.name);
+
+const $player1 = document.createElement('div');
+$player1.innerText='Scorpion';
+$player1.classList.add('player1');
+
+const $root=document.querySelector('div');
+$root.appendChild($player1)
